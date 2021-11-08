53d54701af10fe6513384923eaadfd19bc1c756c
František Šitner <sitner@cortex.cz>
2021-11-08 11:12:59 +0100

    Changed url for api spec repository and script for generating client

---
+++ bin/generate-client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROJECT_DIRECTORY=$(dirname $(cd `dirname $0` && pwd))
+docker run --rm -v $PROJECT_DIRECTORY/deps:/local -e https_proxy=$https_proxy php-server:5.6 composer update --no-dev --optimize-autoloader -d /local
+
+docker run --rm -v $PROJECT_DIRECTORY:/local swaggerapi/swagger-codegen-cli-v3:3.0.26 generate -i /local/deps/vendor/carecloud/api-spec/_build/openapi.yaml -l php -o /local -t /local/resources/php -c /local/client-config.json
+
+docker run --rm -v $PROJECT_DIRECTORY:/local php-server:5.6 sed -i -e 's/AnyOfEventData/\\CareCloud\\Model\\AnyOfEventData/g' /local/lib/Model/Event.php
+docker run --rm -v $PROJECT_DIRECTORY:/local php-server:5.6 sed -i -e 's/AnyOfPropertyRecordPropertyValue/\\CareCloud\\Model\\AnyOfPropertyRecordPropertyValue/g' /local/lib/Model/PropertyRecord.php
